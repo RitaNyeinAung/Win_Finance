2d6e5472ef1a92e534dd4608d597ef41bebc1bef
RitaNyeinAung <ritanyeinaung.ec@gmail.com>
2022-10-04 16:57:35 +0630

    modify upload document

--- src/views/UploadDocument/UploadStepOne.vue
+++ src/views/UploadDocument/UploadStepOne.vue
@@ -2,23 +2,20 @@
       <div class="q-pa-md">
          <div class="q-gutter-lg">
             <p class="title_color">Employee Informations</p>
-            <div class="q-mb-md q-mt-lg">
-               <span>Years of Services</span>
-               <div class="q-mt-md">
-                  <div style="display: flex">
-                     <label class="text-center q-mr-sm" style="width: 50%;">NRC Front</label>
-                     <label class="text-center" style="width: 50%;">NRC Back</label>
-                  </div>
-                  <div style="display: flex">
-                     <q-uploader
-                        url="http://localhost:4444/upload"
-                        style="max-width: 300px"
-                     />
-                     <q-uploader
-                        url="http://localhost:4444/upload"
-                        style="max-width: 300px"
-                     />
-                  </div>
+            <div class="q-mt-lg">
+               <div style="display: flex">
+                  <label class="text-center q-mb-sm" style="width: 50%;">NRC Front</label>
+                  <label class="text-center q-mb-sm" style="width: 50%;">NRC Back</label>
+               </div>
+               <div style="display: flex; justify-content: space-between;">
+                  <q-uploader
+                     url="http://localhost:4444/upload"
+                     style="width: 47%"
+                  />
+                  <q-uploader
+                     url="http://localhost:4444/upload"
+                     style="width: 47%"
+                  />
                </div>
             </div>
          </div>
@@ -52,6 +49,45 @@ export default {
 .items-start {
     align-items: flex-start;
     margin-top: 8px !important;
+}
+.q-uploader__header {
+    position: relative;
+    border-top-left-radius: inherit;
+    border-top-right-radius: inherit;
+    background-color: #fff !important;
+    color: #333 !important;
+}
+.q-uploader__header-content {
+    padding: 4px !important;
+    align-items: center !important;
+}
+.q-uploader__subtitle {
+    font-size: 12px;
+    line-height: 18px;
+    display: none !important;
+}
+.q-uploader__title {
+    font-size: 14px;
+    font-weight: bold;
+    line-height: 18px;
+    word-break: break-word;
+    display: none !important;
+}
+.q-uploader__list {
+    position: relative;
+    border-bottom-left-radius: inherit;
+    border-bottom-right-radius: inherit;
+    padding: 0 !important;
+    min-height: 80px;
+    flex: 1 1 auto;
+}
+.q-uploader__file--img {
+    color: #fff;
+    height: 80px !important;
+    min-width: 0 !important;
+    background-position: 50% 50%;
+    background-size: cover;
+    background-repeat: no-repeat;
 }
 </style>
 
---
+++ src/views/UploadDocument/UploadStepTwo.vue
@@ -0,0 +1,32 @@
+<template>
+      <div class="q-pa-md">
+         <div class="q-gutter-lg">
+            <p class="title_color">Guarantor_Family Informations</p>
+            <div class="q-mt-lg">
+               <div style="display: flex">
+                  <label class="text-center q-mb-sm" style="width: 50%;">Copy of NRC Front</label>
+                  <label class="text-center q-mb-sm" style="width: 50%;">Copy of NRC Back</label>
+               </div>
+               <div style="display: flex; justify-content: space-between;">
+                  
+               </div>
+            </div>
+         </div>
+      </div>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+      }
+   },
+}
+</script>
+
+<style scoped>
+.title_color {
+   color: #005198;
+   font-size: 16px;
+}
+</style>
